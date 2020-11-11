9f41ccf9ecae0e9e467718922c53f5ab707cf834
Roman Odermatt <roman@eyevip.ch>
2020-11-11 07:57:53 +0100

    clean up lib/pg

--- lib/pg/insert_vals.go
+++ lib/pg/insert_vals.go
@@ -8,14 +8,20 @@ import (
 type inserVals struct {
 	values [][]interface{}
 }
+
+// NewInsert creates a store for rows that
+// will be inserted with an SQL-statement
 func NewInsert() *inserVals {
 	return new(inserVals)
 }
+
+// Add appends one or more rows to the insert store
 func (v *inserVals) Add(values ...[]interface{}) error {
 	v.values = append(v.values, values...)
 	return nil
 }
 
+// Values returns all stored rows in a single-dimension array to use with the sql package
 func (v *inserVals) Values() []interface{} {
 	values := []interface{}{}
 	for _, r := range v.values {
@@ -23,8 +29,10 @@ func (v *inserVals) Values() []interface{} {
 	}
 	return values
 }
+
+// String returns the VALUES-Clause with the size of given value-rows
 func (v *inserVals) String() string {
-	base := 1;
+	base := 1
 	mRows := []string{}
 	for _, r := range v.values {
 		mRows = append(mRows, fmt.Sprintf("(%s)", joinN(len(r), &base, ", ")))
