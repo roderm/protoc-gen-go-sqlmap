4e54b82ac0332f77f88be49ac307851cdf63eb48
Roman Odermatt <roman@eyevip.ch>
2020-12-22 12:28:01 +0100

    remove println

--- lib/pg/helper.go
+++ lib/pg/helper.go
@@ -7,7 +7,6 @@ import (
 
 func joinN(n int, paramBase *int, sep string) string {
 	arr := make([]string, n)
-	fmt.Println(len(arr))
 	for i := range arr {
 		*paramBase++
 		arr[i] = fmt.Sprintf("$%d", *paramBase)
