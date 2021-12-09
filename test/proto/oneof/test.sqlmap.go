5f1bca14c29fd7a553336ed9c4d70efc8bf44bce
Roman Odermatt <roman@eyevip.ch>
2021-12-09 13:59:30 +0100

    regenerate for goldenfile for oneof

--- test/proto/oneof/test.sqlmap.go
+++ test/proto/oneof/test.sqlmap.go
@@ -24,101 +24,6 @@ func NewTestStore(conn *sql.DB) *TestStore {
 	return &TestStore{conn}
 }
 
-func (m *Hardware) Scan(value interface{}) error {
-	buff, ok := value.([]byte)
-	if !ok {
-		return fmt.Errorf("Failed %+v", value)
-	}
-	m.ProductID = string(buff)
-	return nil
-}
-
-func (m *Hardware) Value() (driver.Value, error) {
-	return m.ProductID, nil
-}
-
-func (m *Hardware) GetIdentifier() interface{} {
-	return m.ProductID
-}
-
-type queryHardwareConfig struct {
-	Store        *TestStore
-	filter       pg.Where
-	beforeReturn []func(map[interface{}]*Hardware) error
-	cb           []func(*Hardware)
-	rows         map[interface{}]*Hardware
-}
-
-type HardwareOption func(*queryHardwareConfig)
-
-func HardwareFilter(filter pg.Where) HardwareOption {
-	return func(config *queryHardwareConfig) {
-		if config.filter == nil {
-			config.filter = filter
-		} else {
-			config.filter = pg.AND(config.filter, filter)
-		}
-	}
-}
-
-func HardwareOnRow(cb func(*Hardware)) HardwareOption {
-	return func(s *queryHardwareConfig) {
-		s.cb = append(s.cb, cb)
-	}
-}
-
-func (s *TestStore) Hardware(ctx context.Context, opts ...HardwareOption) (map[interface{}]*Hardware, error) {
-	config := &queryHardwareConfig{
-		Store:  s,
-		filter: pg.NONE(),
-		rows:   make(map[interface{}]*Hardware),
-	}
-	for _, o := range opts {
-		o(config)
-	}
-	err := s.selectHardware(ctx, config.filter, func(row *Hardware) {
-		config.rows[row.ProductID] = row
-		for _, cb := range config.cb {
-			cb(row)
-		}
-	})
-	if err != nil {
-		return config.rows, err
-	}
-
-	for _, cb := range config.beforeReturn {
-		err = cb(config.rows)
-		if err != nil {
-			return config.rows, err
-		}
-	}
-	return config.rows, nil
-}
-func (s *TestStore) selectHardware(ctx context.Context, filter pg.Where, withRow func(*Hardware)) error {
-	where, vals := pg.GetWhereClause(filter, nil)
-	stmt, err := s.conn.PrepareContext(ctx, ` 
-	SELECT "product_id", "product_serial" 
-	FROM "tbl_hardware"
-	`+where)
-	if err != nil {
-		return err
-	}
-	cursor, err := stmt.QueryContext(ctx, vals...)
-	if err != nil {
-		return err
-	}
-	defer cursor.Close()
-	for cursor.Next() {
-		row := new(Hardware)
-		err := cursor.Scan(&row.ProductID, &row.Serial)
-		if err != nil {
-			return err
-		}
-		withRow(row)
-	}
-	return nil
-}
-
 func (m *Product) Scan(value interface{}) error {
 	buff, ok := value.([]byte)
 	if !ok {
@@ -392,6 +297,101 @@ func (s *TestStore) selectSoftware(ctx context.Context, filter pg.Where, withRow
 	for cursor.Next() {
 		row := new(Software)
 		err := cursor.Scan(&row.ProductID, &row.Version)
+		if err != nil {
+			return err
+		}
+		withRow(row)
+	}
+	return nil
+}
+
+func (m *Hardware) Scan(value interface{}) error {
+	buff, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("Failed %+v", value)
+	}
+	m.ProductID = string(buff)
+	return nil
+}
+
+func (m *Hardware) Value() (driver.Value, error) {
+	return m.ProductID, nil
+}
+
+func (m *Hardware) GetIdentifier() interface{} {
+	return m.ProductID
+}
+
+type queryHardwareConfig struct {
+	Store        *TestStore
+	filter       pg.Where
+	beforeReturn []func(map[interface{}]*Hardware) error
+	cb           []func(*Hardware)
+	rows         map[interface{}]*Hardware
+}
+
+type HardwareOption func(*queryHardwareConfig)
+
+func HardwareFilter(filter pg.Where) HardwareOption {
+	return func(config *queryHardwareConfig) {
+		if config.filter == nil {
+			config.filter = filter
+		} else {
+			config.filter = pg.AND(config.filter, filter)
+		}
+	}
+}
+
+func HardwareOnRow(cb func(*Hardware)) HardwareOption {
+	return func(s *queryHardwareConfig) {
+		s.cb = append(s.cb, cb)
+	}
+}
+
+func (s *TestStore) Hardware(ctx context.Context, opts ...HardwareOption) (map[interface{}]*Hardware, error) {
+	config := &queryHardwareConfig{
+		Store:  s,
+		filter: pg.NONE(),
+		rows:   make(map[interface{}]*Hardware),
+	}
+	for _, o := range opts {
+		o(config)
+	}
+	err := s.selectHardware(ctx, config.filter, func(row *Hardware) {
+		config.rows[row.ProductID] = row
+		for _, cb := range config.cb {
+			cb(row)
+		}
+	})
+	if err != nil {
+		return config.rows, err
+	}
+
+	for _, cb := range config.beforeReturn {
+		err = cb(config.rows)
+		if err != nil {
+			return config.rows, err
+		}
+	}
+	return config.rows, nil
+}
+func (s *TestStore) selectHardware(ctx context.Context, filter pg.Where, withRow func(*Hardware)) error {
+	where, vals := pg.GetWhereClause(filter, nil)
+	stmt, err := s.conn.PrepareContext(ctx, ` 
+	SELECT "product_id", "product_serial" 
+	FROM "tbl_hardware"
+	`+where)
+	if err != nil {
+		return err
+	}
+	cursor, err := stmt.QueryContext(ctx, vals...)
+	if err != nil {
+		return err
+	}
+	defer cursor.Close()
+	for cursor.Next() {
+		row := new(Hardware)
+		err := cursor.Scan(&row.ProductID, &row.Serial)
 		if err != nil {
 			return err
 		}
